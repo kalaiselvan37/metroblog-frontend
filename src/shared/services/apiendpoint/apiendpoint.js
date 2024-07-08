ac834bbda85f08b57fb92fe12b43b0b22ec89edc
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-07-08 20:34:30 +0530

    changes done

--- src/shared/services/apiendpoint/apiendpoint.js
+++ src/shared/services/apiendpoint/apiendpoint.js
@@ -1,6 +1,8 @@
 const apiurl=()=>{
    
-    var url="http://192.168.0.26:5000";
+    // var url="http://192.168.0.26:5000";
+    var url="http://metroblog-backend.vercel.app"
+
     return url;
 }
 export default apiurl;
